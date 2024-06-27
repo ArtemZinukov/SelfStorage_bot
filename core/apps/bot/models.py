be28b6cfe72bf834dc0efef9c2270e4bb078b7d2
Aleksey525 <alex_tolchin@mail.ru>
2024-06-27 07:16:55 +0300

    Добавил поле email в модель Order, подключил админку

--- core/apps/bot/models.py
+++ core/apps/bot/models.py
@@ -6,6 +6,7 @@ class Order(models.Model):
     address = models.CharField(verbose_name='Адрес', max_length=200, blank=True)
     phone_number = models.CharField(verbose_name='Номер телефона', max_length=12, blank=True)
     date = models.CharField(verbose_name='Дата', max_length=20, blank=True)
+    email = models.CharField(verbose_name='E-mail', max_length=30, blank=True)
 
 
 
