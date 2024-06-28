816417fd31e55c451f22f5d6526ff2576e34e4a4
Aleksey525 <alex_tolchin@mail.ru>
2024-06-28 15:12:29 +0300

    настроил папку MEDIA и templates

--- core/apps/bot/views.py
+++ core/apps/bot/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from core.apps.bot.models import Order
+
+
+def image_page(request):
+    data = Order.objects.all()
+    return render(request, 'image_page.html', {'data': data})
 
 # Create your views here.
+
